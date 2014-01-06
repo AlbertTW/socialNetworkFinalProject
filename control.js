bd696061207b410f014f99a03823657453eb5ad7
YU <xyyugi@gmail.com>
2014-01-06 23:50:22 +0800

    拿掉檢測用的alert
    將原先檢測用的alert拿掉
    刪除不需要用到的變數

--- control.js
+++ control.js
@@ -1,30 +1,23 @@
 
 $(document).ready(function(){
 
-	alert('load');
-	var tableNum = 1;
+//	alert('load');
 	var tableTotalNum = $('#tableTotalNum').val();
-	alert(tableTotalNum);
 	
 	
 	$('.resultTable').hide();	// 查詢回來結果的table全部隱藏
-	$('#showFriend_1').show();
+	$('#showFriend_1').show();	// 先show出第一個table
 	
+	// click "More" event
 	$('.showMore').click(function(){
-			alert('in click');
+//			alert('in click');
 			var num;
-			
-			{
-				eval("num = $(this).attr('value');");
-	//			$('.showMore').attr('value');
-				alert(num);
-				eval("$('#showFriend_"+ num + "').hide();");
-				num++;
-				eval("$('#showFriend_"+ num + "').show();");
-			}
+
+			eval("num = $(this).attr('value');");
+			eval("$('#showFriend_"+ num + "').hide();");
+			num++;
+			eval("$('#showFriend_"+ num + "').show();");
 			
 		});
-	
-	
 
 });
